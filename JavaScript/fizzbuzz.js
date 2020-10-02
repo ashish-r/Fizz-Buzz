63f90aaca60621330960e44a323ea9023be96b1a
Phranc <frank.w.taylor88@gmail.com>
2020-10-02 12:04:52 -0400

    I used Number.isInteger to check divisibilty

--- JavaScript/fizzbuzz.js
+++ JavaScript/fizzbuzz.js
@@ -1,20 +1,18 @@
 // If the number is divisible by 3 then print ‘Fizz’.
 // If the number is divisible by 5 then print ‘Buzz’.
 // If the number is divisible by both 3 and 5 then print ‘FizzBuzz’.
-let fizzBuzz = (n) => {
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz');
-        } else if (i % 3 === 0) {
-            console.log('Fizz');
-        } else if (i % 5 === 0) {
-            console.log('Buzz');
-        } else {
-            console.log(i);
-        }
-    }
-}
-fizzBuzz(10)
+//I used Number.isInteger to check divisability. 
 
-//Output
-/* 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz */
+for (var i = 1; i<101; i++) {
+    if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
+        console.log('FizzBuzz')
+    } else if (Number.isInteger(i/3)){
+        console.log('Fizz')
+    } else if (Number.isInteger(i/5)){
+        console.log('Buzz')   
+    } else {
+        console.log(i)
+    }
+    
+};
+
