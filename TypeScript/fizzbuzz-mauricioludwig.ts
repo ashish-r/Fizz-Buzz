68b2ddc3a8a35b8b8301c833bbc5394d13935d3b
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-10-03 00:31:41 +0200

    add ts implementation

---
+++ TypeScript/fizzbuzz-mauricioludwig.ts
@@ -0,0 +1,17 @@
+/**
+ * * FizzBuzz implementation in TypeScript
+ */
+
+const fizzBuzz = (): void => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+};
