aa7f6ce051fe0e95430ae30cc5b801b2cc8cbb8a
Ghiurcuta Andrei <ghiurcutaandrei@gmail.com>
2020-10-03 01:59:09 +0300

    Added another implementation for FizzBuzz in C#

---
+++ C#/fizz_buzz_C_sharp_method2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string str = "";
+                if (i % 3 == 0)
+                {
+                    str += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    str += "Buzz";
+                }
+                if (str.Length == 0)
+                {
+                    str = i.ToString();
+                }
+                Console.WriteLine(str);
+
+            }
+        }
+    }
+}
