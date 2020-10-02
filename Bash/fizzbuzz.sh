0c0801f9d6d9281deec9ba166d6b298311ce1af3
Palak305 <72197687+Palak305@users.noreply.github.com>
2020-10-02 23:03:14 +0530

    Create fizzbuzz.sh

---
+++ Bash/fizzbuzz.sh
@@ -0,0 +1,13 @@
+# Hello World Program in Bash Shell
+
+for ((i=1;i<=100;i++)); do
+    if ! ((i%15)); then
+        echo FizzBuzz
+    elif ! ((i%3)); then
+        echo Fizz
+    elif ! ((i%5)); then
+        echo Buzz
+    else
+        echo $i
+    fi;
+done
