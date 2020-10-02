527dc86211891bfcb6d6ecb4b49d97350c859b8a
Vipen Loka <vipenl26@gmail.com>
2020-10-02 18:05:31 +0530

    added Python and C++ file for FizzBuzz

---
+++ C++/fizzbuzz.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	cout << "Enter a number\n";
+
+	int n;cin >> n;
+
+	for(int i=1;i<=n;i++){
+		if(i%5 ==0 && i%3==0){
+			cout << "FizzBuzz, ";
+		}
+		else if(i%3==0){
+			cout << "Fizz, ";
+		}
+		else if(i%5==0){
+			cout << "Buzz, ";
+		}
+		else{
+			cout << i << ", ";
+		}
+
+		if(i%10==0)cout << "\n";
+
+	}
+
+
+	return 0;	
+}
