174cb0deb5849d7d7a5e2a8a7f5effd6a42b36e0
Tejas shah <shahtejas2000@gmail.com>
2020-10-02 18:30:34 +0530

    Create fizzbuzz.c

---
+++ C/fizzbuzz.c
@@ -0,0 +1,25 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i=1;i<=100;i++)
+    {
+        if(i%5 ==0 && i%3==0){
+			printf("FizzBuzz, ");
+		}
+		else if(i%3==0){
+			printf("Fizz, ");
+		}
+		else if(i%5==0){
+			printf("Buzz, ");
+		}
+		else{
+			printf("%d, ",i);
+		}
+    }
+    
+
+    return 0;
+}
