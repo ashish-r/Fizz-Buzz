9f32fa5beb58fc9508e7ba8210d53171ddfd0d46
Shrey Raj <53911063+shreyraj2002@users.noreply.github.com>
2020-10-02 19:42:19 +0530

    fizzbuzz.dart

---
+++ Dart/fizz_buzz.dart
@@ -0,0 +1,21 @@
+// Language:       Dart
+// Web site:       https://www.dartlang.org/
+// Requires:       Download https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/debian_wheezy/dart_1.20.1-1_amd64.deb
+//                          dpkg -i dart_1.20.1-1_amd64.deb
+
+void main() {
+    for (var i = 1; i <= 100; i ++) {
+        if (i % 15 == 0) {
+            print("FizzBuzz");
+        }
+        else if (i % 3 == 0) {
+            print("Fizz");
+        }
+        else if (i % 5 == 0) {
+            print("Buzz");
+        }
+        else {
+            print(i);
+        }
+    }
+}
