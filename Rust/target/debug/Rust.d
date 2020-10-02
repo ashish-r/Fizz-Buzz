7325cbd416dd1e96eb00de0c53ddd1a4455d450f
ProCode2 <bpro249@gmail.com>
2020-10-02 18:05:40 +0530

    Added fizz-buzz in Rust

---
+++ Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+/Users/pradiptaghoshal/Desktop/projects/hacktoberfest-2020/Fizz-Buzz/Rust/target/debug/Rust: /Users/pradiptaghoshal/Desktop/projects/hacktoberfest-2020/Fizz-Buzz/Rust/src/main.rs
---
+++ Rust/target/debug/deps/Rust.d
@@ -0,0 +1,5 @@
+/Users/pradiptaghoshal/Desktop/projects/hacktoberfest-2020/Fizz-Buzz/Rust/target/debug/deps/Rust: src/main.rs
+
+/Users/pradiptaghoshal/Desktop/projects/hacktoberfest-2020/Fizz-Buzz/Rust/target/debug/deps/Rust.d: src/main.rs
+
+src/main.rs:
