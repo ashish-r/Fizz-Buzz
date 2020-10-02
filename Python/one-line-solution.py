aece354a780ca563a2af259c92bf81753bbc62df
Mohammed Ijas <mohammedijas619@gmail.com>
2020-10-02 18:43:16 +0530

    one line python solution

--- Python/one-line-solution.py
+++ Python/one-line-solution.py
@@ -1,3 +1,3 @@
 # Python 1 line code for FizzBuzz
 # Author: @mohammed-ijas
-print(*map(lambda v: 'Fizz'*(not v%3)+'Buzz'*(not v%5) or v, range(1,101)),sep=' ')
+print(*map(lambda v: 'Fizz'*(not v%3)+'Buzz'*(not v%5) or v, range(1,101)),sep=', ')
