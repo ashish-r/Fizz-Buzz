6b17d96604ddc6270c901d432456bda20d6629e7
premakhil <akhilpchand@gmail.com>
2020-10-03 13:13:28 +0530

    Added Fizz_Buzz to python folder

---
+++ Python/Fizz_buzz_akhilprem.py
@@ -0,0 +1,16 @@
+def Fizz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print('Fizz')
+
+        elif i % 5 == 0:
+            print('Buzz')
+
+        elif i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+
+        else:
+            print(i)
+
+
+Fizz()
