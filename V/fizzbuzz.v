b32ef7b5522f5c7680a0421128bf65657b3b4e35
Mariano Córdoba <marianocordoba039@gmail.com>
2020-10-03 20:08:16 -0300

    FizzBuzz implementation in V

---
+++ V/fizzbuzz.v
@@ -0,0 +1,13 @@
+fn main() {
+	for n in 1..100 {
+		if n % 3 == 0 && n % 5 == 0 {
+			println('FizzBuzz')
+		} else if n % 3 == 0 {
+			println('Fizz')
+		} else if n % 5 == 0 {
+			println('Buzz')
+		} else {
+			println("$n")
+		}
+	}
+}
