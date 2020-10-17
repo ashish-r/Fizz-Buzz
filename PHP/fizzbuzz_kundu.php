bd17c0249951a233c4e19b27deed789eaa8f1e81
sebabrata <sebabratakundu9@gmail.com>
2020-10-17 22:11:41 +0530

    apply OOPs concept in php to do the FizzBuzz task

--- PHP/fizzbuzz_kundu.php
+++ PHP/fizzbuzz_kundu.php
@@ -8,31 +8,36 @@ class fizzBazz{
     {
         $this->n = $n;
 
-        for ($this->i = 0; $this->i < $this->n; $this->i++) {
-            $this->response = check::mul_of_3($this->i);
-            if ($this->response) {
-                echo "Fizz";
-            }
-
-            echo $this->i;
+        for ($this->i = 1; $this->i < $this->n; $this->i++) {
+            $this->response = check::mul($this->i);
+            echo $this -> response.", ";
         }
     }
 }
 
 class check{
     private static $i;
-    public static function mul_of_3($i)
+    public static $result;
+    public static function mul($i)
     {
         self::$i = $i;
-        if (self::$i % 3 == 0) {
-            return true;
-        } else {
-            return false;
+        if(self::$i % 3 == 0 && self::$i % 5 == 0){
+            self::$result = "FizzBuzz";
         }
+        else if(self::$i % 3 == 0) {
+            self::$result = "Fizz";
+        } 
+        else if(self::$i % 5 == 0){
+            self::$result = "Buzz";
+        } 
+        else{
+            self::$result = self::$i;
+        }
+
+        return self::$result;
     }
 }
 
 new fizzBazz(100);
-
 
 ?>
